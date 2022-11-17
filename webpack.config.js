eebb16764934b2c9b20be920b951f062c44a620b
Jason Marshall <JasonMarshallSelene@gmail.com>
2022-11-16 20:56:24 -0500

    getting things going

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('profile', './assets/app.profile.js')
+    .addEntry('kunst', './assets/app.kunst.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
