48389573c64173b845c816a0896eaffce672bf22
Jason Marshall <JasonMarshallSelene@gmail.com>
2022-11-16 22:35:24 -0500

    added the site description to the settings

--- src/Form/SettingType.php
+++ src/Form/SettingType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,6 +16,7 @@ class SettingType extends AbstractType
         $builder
             ->add('sitename', TextType::class, ['label' => 'Site Name'])
             ->add('sitenick', TextType::class, ['label' => 'Site NickName'])
+            ->add('sitedesc', TextareaType::class, ['label' => 'Site Description'])
             ->add('save', SubmitType::class, ['label' => 'Save Settings'])
         ;
     }
