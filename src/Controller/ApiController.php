2f6836de612fec46899ffbd623a9d6e61380f760
Jason Marshall <JasonMarshallSelene@gmail.com>
2022-11-18 14:11:05 -0500

    feed controller started

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Setting;
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends AbstractController
 {
     #[Route('/api', name: 'app_api')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('api/index.html.twig', [
-            'controller_name' => 'ApiController',
+        $repo = $doctrine->getRepository(Setting::class);
+        $u = $doctrine->getRepository(User::class)->findAll();
+        $users = [];
+        foreach ($u as $v) {
+            $users[] = $v->getUsername();
+        }
+        // Any of the site settings are hard coded here because why not?
+        $siteName = $repo->findOneByName('sitename');
+        $siteNick = $repo->findOneByName('sitenick');
+        $siteDesc = $repo->findOneByName('sitedesc');
+        $response = new JsonResponse([
+            'SiteName' => $siteName->getValue(),
+            'SiteNick' => $siteNick->getValue(),
+            'SiteDesc' => $siteDesc->getValue(),
+            'Users' => $users,
         ]);
+
+        return $response;
     }
 }
--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FeedController extends AbstractController
 {
-    #[Route('/feed/', name: 'app_feed')]
-    public function index(): Response
+    #[Route('/feed/{username}', name: 'app_feed')]
+    #[Entity('User', options: ['username' => 'username'])]
+    public function index(User $user): Response
     {
         return $this->render('feed/index.html.twig', [
             'controller_name' => 'FeedController',
