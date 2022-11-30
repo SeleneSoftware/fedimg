4a125fa75848d5af51e7b56409509b754b5bdc73
Jason Marshall <JasonMarshallSelene@gmail.com>
2022-11-29 21:15:07 -0500

    adding a license

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default')]
-    public function index(): Response
+    public function index(UserRepository $users): Response
     {
+        $u = $users->findAll();
+        $posts = [];
+        foreach ($u as $i) {
+            $posts = array_merge($posts, $i->getPosts()->getValues());
+        }
+
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'theme' => 'kunst',
+            'feed' => $posts,
+            // 'userCode' => $username.'@'.$sitecode,
         ]);
     }
 }
